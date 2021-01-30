a469ea940e083669b3a435dc11265d3a8fac60e5
김승중 <31948929+Ipsilo@users.noreply.github.com>
2021-01-30 13:07:42 +0900

    유니티 맵 에셋 추가
    맵을 구성하는 에셋입니다

---
+++ Assets/OS_Environment/Modular sci-fi Residential sector A/_Shared/Scripts/AutoDoorController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OSContentDemo
+{
+    public class AutoDoorController : MonoBehaviour
+    {
+        [SerializeField] private Animator anim;
+        [SerializeField] private List<Transform> inTriggerZone;
+
+        void Awake()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("Player"))
+            {
+                inTriggerZone.Add(other.transform);
+                Open();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                inTriggerZone.Remove(other.transform);
+            }
+
+            CheckNeedClose();
+        }
+
+        private void CheckNeedClose()
+        {
+            if(inTriggerZone.Count<1)
+            {
+                Close();
+            }
+        }
+
+        public void Open()
+        {
+            if(anim!=null)
+            {
+                anim.SetBool("Open", true);
+            }
+        }
+
+        public void Close()
+        {
+            if (anim != null)
+            {
+                anim.SetBool("Open", false);
+            }
+        }
+    }
+}
