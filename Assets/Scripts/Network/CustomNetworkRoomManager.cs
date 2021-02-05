bcaa3e0746039c26c064764a3bcce5d8aec07ed0
anheew1 <anheew1@gmail.com>
2021-02-05 23:20:11 +0900

    MultiTest Start Scene Backend

---
+++ Assets/Scripts/Network/CustomNetworkRoomManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CustomNetworkRoomManager : NetworkRoomManager
+{
+    private string serverName;
+    private string userName;
+    private static CustomNetworkRoomManager instance;
+
+    public override void Awake() 
+    {
+        base.Awake();
+
+        if (instance == null && this.dontDestroyOnLoad) // 이게 안먹힌경우
+        {
+            instance = this;
+        }
+        else
+        {
+            if (this != instance)
+            {
+                Destroy(instance.gameObject);
+                instance = this; // instance 시작 스크린으로 돌아올때 새걸로 교체
+            }
+        }
+    }
+
+    private void SetNicknameAtRoomPlayer()
+    {
+
+    }
+
+
+    public override void OnRoomStartHost()
+    {
+        SetNicknameAtRoomPlayer();
+    }
+    public override void OnRoomClientEnter()
+    {
+        SetNicknameAtRoomPlayer();
+    }
+
+}
