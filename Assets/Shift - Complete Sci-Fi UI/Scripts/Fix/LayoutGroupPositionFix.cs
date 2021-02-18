44d0099b6ee39bc6375f33b5a342237ed8e7a7ed
anheew1 <anheew1@gmail.com>
2021-02-18 23:32:51 +0900

    Add Assets

---
+++ Assets/Shift - Complete Sci-Fi UI/Scripts/Fix/LayoutGroupPositionFix.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Michsky.UI.Shift
+{
+    public class LayoutGroupPositionFix : MonoBehaviour
+    {
+        public bool forceRect = true;
+
+        public void FixPos()
+        {
+            if (forceRect == true)
+            {
+                try
+                {
+                    LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+                }
+
+                catch { }
+            }
+
+            else
+            {
+                gameObject.SetActive(false);
+                gameObject.SetActive(true);
+            }
+        }
+    }
+}
