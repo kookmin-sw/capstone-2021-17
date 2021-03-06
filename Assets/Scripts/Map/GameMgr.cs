28fcdbb23ebed9b57aab5a6402cd72210cbe660b
김승중 <31948929+pow8956@users.noreply.github.com>
2021-03-06 16:50:04 +0900

    스폰포인트 및 랜덤스폰 구현
    아이템박스가 지정된 스폰포인트 중에서 랜덤한 위치에 생성되도록 구현

--- Assets/Scripts/Map/GameMgr.cs
+++ Assets/Scripts/Map/GameMgr.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] points;
+    public GameObject itembox;
+    public int boxcount;
+
     void Start()
     {
-        
+        List<Transform> temp_list = new List<Transform>(points);
+        int idx;
+        for (int i =0; i<boxcount; ++i)
+        {
+            idx = Random.Range(0, temp_list.Count);
+            Instantiate(itembox, temp_list[idx].position, Quaternion.identity);
+            temp_list.Remove(temp_list[idx]);
+            Debug.Log(idx+"위치에 생성");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
