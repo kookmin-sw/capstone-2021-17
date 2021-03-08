6eac4eae4447e9d9366d79a3134096d992ad8c05
anheew1 <anheew1@gmail.com>
2021-03-08 19:38:36 +0900

    Update NetManager.cs

--- Assets/Scripts/Network/NetManager.cs
+++ Assets/Scripts/Network/NetManager.cs
@@ -15,6 +15,7 @@ public class NetManager : NetworkRoomManager
     public override void Awake() 
     {
         base.Awake();
+
         if(instance == null)
         {
             instance = this;
