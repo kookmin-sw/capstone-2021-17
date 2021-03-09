9e2bd858c83355f11502eebf16c767d8f55578b7
김승중 <31948929+pow8956@users.noreply.github.com>
2021-03-09 14:25:38 +0900

    랜덤스폰 중복 제거+에셋추가
    아이템박스가 중복된 위치에 생성되지 않도록 조정

--- Assets/Scripts/Map/GameMgr.cs
+++ Assets/Scripts/Map/GameMgr.cs
@@ -7,22 +7,48 @@ public class GameMgr : MonoBehaviour
     public Transform[] points;
     public GameObject itembox;
     public int boxcount;
+    public int spawnpointcount;
 
     void Start()
     {
         List<Transform> temp_list = new List<Transform>(points);
-        int idx;
-        for (int i =0; i<boxcount; ++i)
+        int[] idx = getRandomInt(boxcount, 0, spawnpointcount);
+        for (int i=0; i<boxcount; i++)
         {
-            idx = Random.Range(0, temp_list.Count);
-            Instantiate(itembox, temp_list[idx].position, Quaternion.identity);
-            temp_list.Remove(temp_list[idx]);
-            Debug.Log(idx+"위치에 생성");
+            Instantiate(itembox, temp_list[idx[i]].position, Quaternion.identity);
+            Debug.Log(idx[i]+"위치에 아이템박스 생성");
+            //temp_list.Remove(temp_list[idx[i]]);
         }
     }
 
     void Update()
     {
 
+    }
+
+    public int[] getRandomInt(int length, int min, int max)
+    {
+        int[] randArray = new int[length];
+        bool isSame;
+
+        for (int i=0; i<length; i++)
+        {
+            while (true)
+            {
+                randArray[i] = Random.Range(min, max);
+                isSame = false;
+
+                for (int j=0; j<i; j++)
+                {
+                    if (randArray[j] == randArray[i])
+                    {
+                        isSame = true;
+                        break;
+                    }
+                }
+                if (!isSame) break;
+            }
+        }
+        return randArray;
     }
 }
