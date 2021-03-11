e9d4412bf945011f1d9729ca399a1cdc47e9281d
anheew1 <anheew1@gmail.com>
2021-03-11 16:48:25 +0900

    Add NetGamePlayer Scripts to Player Prefab

--- Assets/Scripts/Network/NetGamePlayer.cs
+++ Assets/Scripts/Network/NetGamePlayer.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+
 public class NetGamePlayer : NetworkBehaviour
 {
-    private NetworkIdentity identity;
+
+    public ThirdCamera thirdCamera;
+    public ThirdPersonCharacter character;
+    public ThirdPersonUserControl control;
+    public NetworkIdentity identity;
+
 
     public override void OnStartClient()
     {
         base.OnStartClient();
 
         identity = GetComponent<NetworkIdentity>();
+        
+        
+    }
+
+    public void MoveCharacter(Vector3 move, bool crouch, bool jump)
+    {
+        if (isLocalPlayer)
+        {
+            AttribMoveCharacter(move, crouch, jump);
+        }
+            
+    }
+
+    [Command]
+    public void AttribMoveCharacter(Vector3 move, bool crouch, bool jump)
+    {
+
+        character.Move(move, crouch, jump);
     }
 }
