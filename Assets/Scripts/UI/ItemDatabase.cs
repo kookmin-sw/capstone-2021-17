e01d17bd32372d5d997e2a915db2c45059126ca3
leegahee <rk08gml11@naver.com>
2021-03-17 00:03:37 +0900

    아이템창 표시
    아이템 획득시 종류에 맞는 아이콘 창에 표시

--- Assets/Scripts/UI/ItemDatabase.cs
+++ Assets/Scripts/UI/ItemDatabase.cs
@@ -1,23 +1,65 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
 
-public class ItemDatabase : MonoBehaviour {
-    public static ItemDatabase instance;
-    public List<Item> items = new List<Item>();
+public class ItemDatabase : MonoBehaviour 
+{
+    public Image[] slot = new Image[4];
+    public Sprite[] sprites = new Sprite[2];
+    private Sprite thisImage;
+    private bool[] isEmpty = new bool[4];
 
-    void Awake()
-    {
-        instance = this;
-    }
 
-	// Use this for initialization
 	void Start () 
     {
-        Add("heal", "체력 1칸 회복", ItemType.Consumption);
-    }    
-	
-void Add(string itemName, string itemDesc, ItemType itemType)
-    {
-        items.Add(new Item(itemName,itemDesc, itemType,Mokup.Load<Sprite>("ItemImages/" + itemName)));
+        for(int i=0; i<4; i++){
+            isEmpty[i] = true;
+        }
     }
+    
+    //힐 아이콘 추가
+    public void AddHeal()
+    {
+        thisImage = sprites[0];
+        AddItem();
+    }
+
+    //아이템 창에 추가
+    public void AddItem()
+    {
+        for(int i=0; i<4; i++)
+        {
+            if(isEmpty[i]==true)
+            {
+                slot[i].sprite = thisImage;
+                slot[i].gameObject.SetActive(true);
+                isEmpty[i] = false;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    //아이템 삭제
+    public void RemoveItem()
+    {
+        for(int i=3; i>=0; i--)
+        {
+            if(isEmpty[i]==false)
+            {
+                slot[i].sprite = null;
+                slot[i].gameObject.SetActive(false);
+                isEmpty[i] = true;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }    
 }
