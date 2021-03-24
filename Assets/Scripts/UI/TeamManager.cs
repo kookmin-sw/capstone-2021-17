41190d6e5fa51459eab87fe3a6db376432807205
leegahee <rk08gml11@naver.com>
2021-03-25 00:00:33 +0900

    플레이어 스크립트 수정
    ingame_multimanager와 플레이어 정보 연동 & 화면에 표시되도록 수정

--- Assets/Scripts/UI/TeamManager.cs
+++ Assets/Scripts/UI/TeamManager.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 public class TeamManager : MonoBehaviour
 {
     public Text[] UI;
-    private int count_player;
-    private string[] p_name;
-    private float[] p_HP;
-    private float[] p_server;
+
+    public static List<NetGamePlayer> Players = new List<NetGamePlayer>(NetManager.PLAYER_MAXNUM);
+    private static List<int> healths = new List<int>();
+    private static List<string> names = new List<string>();
+    private static List<ThirdPersonCharacter.State> states = new List<ThirdPersonCharacter.State>();
+
+    private static int[] server;
+
 
     void Start()
     {
-        count_player = UI.Length;
-        LoadName();
-        LoadServer();
-        p_HP = new float[count_player];
+        InGame_MultiGameManager.GetPlayersNickname();
+        InGame_MultiGameManager.GetPlayersHealth();
+        InGame_MultiGameManager.GetPlayersState();
     }
 
     void Update()
@@ -27,46 +30,28 @@ public class TeamManager : MonoBehaviour
 //텍스트 정보 출력 (이름, 체력, 서버상태)
     private void ShowText()
     {
-        for(int id = 0; id<count_player; id++)
+        for(int id = 0; id<Players.Count-1; id++)
         {
-            UI[id].text = "Name : " + p_name[id] + "\n" + CheckHP(id) + "\n Server : " 
-            + p_server[id];
+            if(InGame_MultiGameManager.isPlayerLeader(id+1))
+            {
+                UI[id].text = "Name : " + names[id+1] + "\n" + healths[id+1] + "\n state : " 
+            + states[id] + "\n < Leader";
+            }
+            else
+            { 
+                UI[id].text = "Name : " + names[id+1] + "\n" + healths[id+1] + "\n state : " 
+                + states[id];
+            }
         }
     }
+/*
+    //서버 상태 간략화
+    private void SimpleServer(){
 
-//이름 가져오기 (임시 목업)
-    private void LoadName()
-    {
-        p_name = new string[count_player];
-        p_name[0] = "dog";
-        p_name[1] = "cat";
-        p_name[2] = "bird";
-    }
+    }*/
 
-    private string CheckHP(int _id)
-    {
-        //hp정보 연동
-        p_HP[_id] = HPManager.hp[_id+1];
+    //사망 또는 게임 클리어 여부
 
-        //사망시 Dead 표시
-        if(p_HP[_id]==0)
-        {
-            return "Dead";
-        }
-        else
-        {
-            return "HP : " + p_HP[_id];
-        }
-    }
-
-    //서버 정보 연동(임시 목업)
-    private void LoadServer()
-    {
-        p_server = new float[count_player];
-        p_server[0] = 33;
-        p_server[1] = 66;
-        p_server[2] = 99;
-    }
         
 
 }
