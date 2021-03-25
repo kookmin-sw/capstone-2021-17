67e557d06e58b70552c7c93115071ade18465e96
leegahee <rk08gml11@naver.com>
2021-03-26 03:12:25 +0900

    스크립트 분리 > IngameUIManager
    스크립트 중 인게임 내에서만 사용되는 스크립트 삭제 및 분리

--- Assets/Scripts/UI/SceneManager.cs
+++ Assets/Scripts/UI/SceneManager.cs
@@ -7,48 +7,32 @@ using UnityEngine.SceneManagement;
 public class SceneManager : MonoBehaviour
 {
     public GameObject[] obj;
-    public Text clearText;
-    bool gameClear;
-    float loadingTime = 5; 
 
-
-    void Start()
-    {
-        loadingTime = 5f;
-        gameClear = false;
-    }
-    void Update()
-    {
-        if(gameClear)
-        {
-            ClearTimeCount();    
-        }
-    }
     //Start, WaitingRoom,Opening, GamePlay
-    public void ChangeStartScene()
+    public static void ChangeStartScene()
     {
         LoadingManager.LoadScene("Start");
     }
-    public void ChangeWaitingRoomScene()
+    public static void ChangeWaitingRoomScene()
     {
         LoadingManager.LoadScene("Loading");
     }
-    public void ChangeOpeningScene()
+    public static void ChangeOpeningScene()
     {
         LoadingManager.LoadScene("Opening");
     }
-    public void ChangeGamePlayScene()
+    public static void ChangeGamePlayScene()
     {
         LoadingManager.LoadScene("GamePlay");
     }
 
-    //버튼 스크립트
     //게임 종료
-    public void GameExit()
+    public static void GameExit()
     {
         Debug.Log("Game Exit");
         Application.Quit();
     }
+
     //버튼 : 켜져있는 경우엔 끄고, 꺼진 경우엔 켜기
     public void OnOff(int i)
     {
@@ -59,22 +43,6 @@ public class SceneManager : MonoBehaviour
         else
         {
             obj[i].SetActive(true);
-        }
-    }
-    //게임 클리어시 활성화
-    public void GameClear()
-    {
-        gameClear = true;
-    }
-    //게임 클리어시 자동으로 로비 이동
-    private void ClearTimeCount()
-    {   
-        loadingTime -= Time.deltaTime;
-        clearText.text = "잠시 후 메인 화면으로 이동합니다.\n" + Mathf.Round(loadingTime) + "초...";
-        
-        if(loadingTime<=0)
-        {
-            ChangeStartScene();
         }
     }
 
