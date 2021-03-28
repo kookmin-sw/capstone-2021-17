489cf3c462493eddce7795ca560dda5910344582
soonmin-kwon <tnsals0709@naver.com>
2021-03-28 23:19:25 +0900

    클래스 이름 변경, Dizzy 애니메이션 추가
    클래스의 기능이 enemy의 애니메이션을 총괄하는 것이기 때문에 이름 변경
    PlayDizzyAnim()으로 Dizzy Idle 애니메이션을 실행

---
+++ Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    
+    //check enemy has Path
+    [SerializeField]
+    private EnemyChase checkAnim;
+    Animator ani;
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // 경로가 있으면 애니메이션 시작.
+    void Update()
+    {       
+        if (checkAnim.hasP)
+        {
+            ani.SetBool("Walk", true);
+        }
+        else
+        {
+            ani.SetBool("Walk", false);
+        }       
+    }
+
+    public void PlayAttAnim()
+    {
+        ani.SetTrigger("Attack");
+    }
+
+    public void PlayDizzyAnim()
+    {
+        ani.SetTrigger("Dizzy");
+    }    
+}
