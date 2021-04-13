6290776ae8f413ab479a200ad5585af6a59060ce
anheew1 <anheew1@gmail.com>
2021-04-13 22:42:44 +0900

    Add Item, Inventory Class

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Item[] Items = new Item[4];
+
+    [SerializeField]
+    private NetGamePlayer netPlayer;
+    [SerializeField]
+    private GameObject Player;
+
+    public bool AddItem(Item newItem)
+    {
+        for(int idx = 0; idx < Items.Length; idx++)
+        {
+            if(Items[idx] == null)
+            {
+                Items[idx] = newItem;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void UseItem(int idx)
+    {
+        Item targetItem = Items[idx];
+
+        if (targetItem == null)
+        {
+            // nothing works..
+        }
+        else if(targetItem.GetType().Name == "HealPack")
+        {
+            HealPack healPack = targetItem.GetComponent<HealPack>();
+            healPack.SetHealObject(healPack.gameObject);
+            healPack.SetPlayerObject(Player);
+            healPack.UseHealPack();
+
+            RemoveItem(idx);
+        }
+
+
+    }
+
+    public void RemoveItem(Item targetItem)
+    {
+        for (int idx = 0; idx < Items.Length; idx++)
+        {
+            if (Items[idx] == targetItem)
+            {
+                Items[idx] = null;
+            }
+        }
+    }
+
+    public void RemoveItem(int idx)
+    {
+        if(Items[idx] != null)
+        {
+            Items[idx] = null;
+        }
+    }
+
+    public bool IsFullInventory()
+    {
+        for (int idx = 0; idx < Items.Length; idx++)
+        {
+            if(Items[idx] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
