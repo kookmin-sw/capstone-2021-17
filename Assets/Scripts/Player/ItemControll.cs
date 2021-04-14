658c5914a761d0dc12e8cc5d7aa6e91071b3bce1
cutejoo <39543050+cutejoo@users.noreply.github.com>
2021-04-14 19:29:32 +0900

    Add Script for item
    아이템 저장, 아이템 확인, 총알 발사 관련 코드 작성

---
+++ Assets/Scripts/Player/ItemControll.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemControll : MonoBehaviour
+{
+    public int Damage;
+    public Transform BulletPos;
+    public GameObject bullet;
+
+}
