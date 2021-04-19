d38fd4a74e5d6a0fe1357c3f77e7b2fd9dd61d1a
leegahee <rk08gml11@naver.com>
2021-04-19 23:44:02 +0900

    메뉴창 단축키 변경
    메뉴창 여는 키를 Q 키에서 Esc 키로 변경.

--- Assets/Scripts/UI/IngameUIManager.cs
+++ Assets/Scripts/UI/IngameUIManager.cs
@@ -58,7 +58,7 @@ public class IngameUIManager : MonoBehaviour
     private void OpenMenu()
     {   
         //메뉴창 열고닫기
-        if(Input.GetKeyDown(KeyCode.Q))
+        if(Input.GetKeyDown(KeyCode.Escape)) //esc키로 메뉴창 열고닫기
         {
             if(menu_UI.activeSelf == false)
             {
