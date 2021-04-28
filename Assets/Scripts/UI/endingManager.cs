f2ffa521f89b229351ee69677b27e91f95e2087a
leegahee <rk08gml11@naver.com>
2021-04-29 00:04:59 +0900

    스크립트 분리 및 수정
    캐릭터 관련 스크립트 분리(=>EndingCharacterManager로 이동)
    게임 클리어, 게임 오버 여부 출력
    캐릭터별 플레이어 닉네임 출력

--- Assets/Scripts/UI/endingManager.cs
+++ Assets/Scripts/UI/endingManager.cs
@@ -2,51 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class endingManager : MonoBehaviour
+public class EndingManager : MonoBehaviour
 {
-    private Animator ending_anim;
-    public Transform character;
-    Vector3 target;
-    // Start is called before the first frame update
-    void Awake()
-    {
-        ending_anim = GetComponent<Animator>();
-        
-    }
+    public Text[] nickname;
+    public Text gameClear;
+
+    public List<NetGamePlayer> Players = new List<NetGamePlayer>(NetManager.PLAYER_MAXNUM);
+    private List<string> names = new List<string>();
+    private List<ThirdPersonCharacter.State> states = new List<ThirdPersonCharacter.State>();
+    private List<string> networkTimes = new List<string>();
+
+    
+    private bool isclear; //게임 클리어 여부
 
     void Start()
     {
-        ending_anim.SetBool("isDead", false);
+        
+        isclear = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (NetworkClient.active || NetworkServer.active)
+        {
+            Players = InGame_MultiGameManager.Players;
+            names = InGame_MultiGameManager.GetPlayersNickname();
+            networkTimes = InGame_MultiGameManager.GetPlayersNetworkTime(); 
+        }
+    }
+
+    //캐릭터 모델 로드 후 캐릭터 상태에 따라 EndingPlayerManager의 islive or lsdead 호출
+
+    private void ShowClearText() //게임 클리어, 게임 오버 여부 출력
+    {
+        //게임 클리어시
+        if(isclear)
+        {
+            gameClear.text = "Game Clear!";
+        }
+        else //게임 오버(전원 사망) 시
+        {
+            gameClear.text = "Game Over...";
+        }
         
     }
 
-    public void isLive()
+    private void ShowPlayerText() //플레이어캐들 닉네임 출력
     {
-        ending_anim.SetBool("isDead", false);
-        target = new Vector3(0, 0, -6.4f);
-        float start = 0;
-        while(transform.position != target){
-            start += Time.deltaTime * 0.1f;
-            transform.position = Vector3.Lerp(transform.position, target, start);
-        }
-        Debug.Log("click live");
-    }
-
-    public void isDead()
-    {
-        target = new Vector3(0, -0.4f, -6.4f);
-        float start = 0;
-        ending_anim.SetBool("isDead", true);
-        while(transform.position != target){
-            start += Time.deltaTime * 0.1f;
-            transform.position = Vector3.Lerp(transform.position, target, start);
-        }
-        Debug.Log("click dead");
+        for(int id = 0; id<Players.Count; id++)
+        {
+            nickname[id].text = names[id];
+        } //왼쪽에서부터 id 순으로 닉네임 출력
     }
 }
