c3e8b331bf7c6dd28287095f2e23916f65991ea6
anheew1 <anheew1@gmail.com>
2021-04-30 22:19:16 +0900

    Sync Mission Clear
    탈출구 문도 열립니다

---
+++ Assets/Scripts/Network/InGame/GameMgrNetBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Mirror;
+
+public class GameMgrNetBehaviour : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(OnClearCountChanged))] 
+    public int missionClearCount;
+
+    private GameMgr gameMgr;
+
+    private void Start()
+    {
+        gameMgr = GameMgr.instance;
+    }
+
+    public void AddMissionClearCount()
+    {
+        CmdAddMissionClearCount();
+    }
+
+    [Command(requiresAuthority =false)]
+    public void CmdAddMissionClearCount()
+    {
+        missionClearCount++;
+    }
+
+    void OnClearCountChanged(int oldValue, int newValue) // SyncVar가 바뀔 때마다 호출됨
+    {
+        missionClearCount = newValue;
+        gameMgr.missionClearCount = missionClearCount;
+
+        if(missionClearCount == gameMgr.missionSpawnCount)
+        {
+            gameMgr.ActiveExitDoor();
+        }
+    } 
+}
