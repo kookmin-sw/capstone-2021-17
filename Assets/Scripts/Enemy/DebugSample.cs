630abe51326a2cd35239a9e461499b8006d3b0bd
soonmin-kwon <tnsals0709@naver.com>
2021-05-01 23:39:03 +0900

    디버깅 기능 수정
    EnemyControl을 사용하지 않고 Enemy를 사용하도록 수정했기 때문에 EnemyControl에서 하던 부분을 Enemy로 수정했습니다.

--- Assets/Scripts/Enemy/DebugSample.cs
+++ Assets/Scripts/Enemy/DebugSample.cs
@@ -6,29 +6,28 @@ public class DebugSample : MonoBehaviour
 {
     [SerializeField]
     private Text text;
-
-    [SerializeField]
-    private EnemyControl enemyControl;
+    
     [SerializeField]
     private AnimationSoundEvent animEvent;
+    [SerializeField]
+    private Enemy enemy;
+
     // Update is called once per frame
     void Update()
     {
 
-        text.text = "State : " + enemyControl.state + "\n\n"
-            + "FindTargetVision : " + enemyControl.findTargetVision + "\n\n"
-            + "IsPatrol : " + enemyControl.isPatrol + "\n\n"
-            + "Distance : " + enemyControl.dis + "\n\n"
-            + "IsAudioEvent : " + enemyControl.findTargetSound + "\n\n"
-            + "Target : " + enemyControl.target + "\n\n"
-            + "WayPoint : " + enemyControl.patrolPos + "\n\n"
-            + "SetTarget : " + enemyControl.enemy.hasPath + "\n\n"
-            + "Sensor : " + enemyControl.turnOnSensor + "\n\n"
+        text.text = "State : " + enemy.enemyStateMachine.currentState + "\n\n"
+            + "FindTargetVision : " + enemy.findTargetVision + "\n\n"           
+            + "Distance : " + enemy.dis + "\n\n"            
+            + "Target : " + enemy.target + "\n\n"
+            + "WayPoint : " + enemy.patrol.patrolPos + "\n\n"
+            + "SetTarget : " + enemy.navMeshAgent.hasPath + "\n\n"
+            + "Sensor : " + enemy.turnOnSensor + "\n\n"
             + "TargetList : ";
 
-        for(int i=0; i< enemyControl.visibleTargets.Count; i++)
+        for(int i=0; i< enemy.visibleTargets.Count; i++)
         {
-            text.text +=  enemyControl.visibleTargets[i] + "\n";
+            text.text +=  enemy.visibleTargets[i] + "\n";
         }            
     }
 }
