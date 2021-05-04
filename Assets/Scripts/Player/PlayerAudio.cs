b0e328d9ad47242513835a0eec4d7eb871f3eccd
soonmin-kwon <tnsals0709@naver.com>
2021-05-04 17:20:56 +0900

    애니메이션 확인 기능 추가
    어떤 애니메이션 클립이 실행되고 있는지 확인하는 기능 추가. 룰에 맞게 변수 이름 수정.

--- Assets/Scripts/Player/PlayerAudio.cs
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -5,72 +5,114 @@ using UnityEngine;
 public class PlayerAudio : MonoBehaviour
 {
     [SerializeField] 
-    AudioSource AudioSource;
-    public AudioClip[] Sound;
+    AudioSource audioSource;
+    public AudioClip[] sound;    
     bool isCrouch; 
     bool isJump;
-    Animator PlayerAnimator;
-    bool Check=false;
-    public bool Checks=false;
+    Animator playerAnimator;
+    bool check=false;
+    public bool checks=false;
     public float jump;
 
 
     void Awake()
     {
-        PlayerAnimator = GetComponent<Animator>();
+        playerAnimator = GetComponent<Animator>();        
     }
 
     void Update()
     {
+        PlayTest();
         CrouchCheck();
         JumpCheck();
     }
 
     void PlayAudio(AudioClip audioclip,float f)
     {
-        AudioSource.PlayOneShot(audioclip,f);
+        audioSource.PlayOneShot(audioclip,f);
     }
 
     void StopAudio()
     {
-        AudioSource.Stop();
+        audioSource.Stop();
     }
 
     void Crouch()
     {
-        if (isCrouch&&Check==false)
+        if (isCrouch&&check==false)
         {   
-            PlayAudio(Sound[1],0.1f);
-            Check=true;
+            PlayAudio(sound[1],0.1f);
+            check=true;
         }
         else if (isCrouch)
         {   
-            PlayAudio(Sound[0],0.1f);
+            PlayAudio(sound[0],0.1f);
         }
     }
 
     void CrouchCheck()
     {
-        isCrouch=PlayerAnimator.GetBool("Crouch");
+        isCrouch=playerAnimator.GetBool("Crouch");
         if(isCrouch==false)
         {
             StopAudio();
-            Check=false;
+            check=false;
         }
     }
 
     void JumpCheck()
     {
-        bool isjump=PlayerAnimator.GetBool("OnGround");
-        if(isjump==false&&Checks==false)
+        bool isjump=playerAnimator.GetBool("OnGround");
+        if(isjump==false&&checks==false)
         {
-            PlayAudio(Sound[2],0.1f);
-            Checks=true;
+            PlayAudio(sound[2],0.1f);
+            checks=true;
 
         }
         else if(isjump)
         {
-            Checks=false;
+            checks=false;
         }
     }
+    
+    //어떤 클립 재생하는지 확인
+    public void Blank()
+    {
+        for(int i=0; i<playerAnimator.GetCurrentAnimatorClipInfo(0).Length; i++)
+        {
+            Debug.Log(playerAnimator.GetCurrentAnimatorClipInfo(0)[i].clip.name);
+        } 
+    }
+
+    /*public void idleTest()
+    {
+        if(PlayerAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name !="HumanoidIdle")
+        {
+            AudioSource.Play();
+        }
+        
+    }*/
+
+    //앉는 동안, 일어서는 동안 등 transition에서 소리나게 하기 
+    //
+    public void PlayTest()
+    {
+        if(playerAnimator.GetAnimatorTransitionInfo(0).IsName("Grounded -> Airborne"))
+        {
+            audioSource.Play();
+        }
+        else if (playerAnimator.GetAnimatorTransitionInfo(0).IsName("Airborne -> Grounded"))
+        {
+            audioSource.Play();
+        }
+        else if (playerAnimator.GetAnimatorTransitionInfo(0).IsName("Grounded -> Crouching"))
+        {
+            audioSource.Play();
+        }
+        else if (playerAnimator.GetAnimatorTransitionInfo(0).IsName("Crouching -> Grounded"))
+        {
+            audioSource.Play();
+        }
+    }
+
 }
