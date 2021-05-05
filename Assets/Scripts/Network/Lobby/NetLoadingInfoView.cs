0e22806fdad1ee4c04d56d423e38eab564ff2c9d
anheew1 <anheew1@gmail.com>
2021-05-06 00:01:14 +0900

    [TestMulti] Set Loading Info about server connection

---
+++ Assets/Scripts/Network/Lobby/NetLoadingInfoView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+using MasterServerToolkit.Games;
+using MasterServerToolkit.Networking;
+using MasterServerToolkit.MasterServer;
+
+public class NetLoadingInfoView : LoadingInfoView
+{
+    public IClientSocket Connection => Mst.Connection;
+
+    protected virtual void Start()
+    {
+        Connection.OnStatusChangedEvent += OnStatusChangedEventHandler;
+        OnStatusChangedEventHandler(Connection.Status);
+    }
+
+    protected virtual void OnStatusChangedEventHandler(ConnectionStatus status)
+    {
+        switch (status)
+        {
+            case ConnectionStatus.Connected:
+                SetLables("Client is connected");
+                break;
+            case ConnectionStatus.Disconnected:
+                SetLables("Client is offline");
+                break;
+            case ConnectionStatus.Connecting:
+                SetLables("Client is connecting");
+                break;
+            default:
+                SetLables("Unknown status");
+                break;
+        }
+    }
+}
