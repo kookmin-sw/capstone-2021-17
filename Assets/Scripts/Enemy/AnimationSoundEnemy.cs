d364b65209abecaa06468ba7c27ea271451dd4fd
soonmin-kwon <tnsals0709@naver.com>
2021-05-08 00:55:36 +0900

    애니메이션 이벤트 메소드 구현
    각 애니메이션에 사용할 이벤트 함수들을 정의했습니다. AttackSound, DizzySound는 각각 Attack, Dizzy 애니메이션에서 사용합니다. SetCollider는 Attack 애니메이션에서 사용해 팔을 휘두를 때만 콜라이더를 키는 방식으로 구현했습니다.

--- Assets/Scripts/Enemy/AnimationSoundEnemy.cs
+++ Assets/Scripts/Enemy/AnimationSoundEnemy.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class AnimationSoundEnemy : MonoBehaviour
 {
-    [SerializeField]
-    AudioSource walkAudio;
+    [SerializeField] private AudioSource audioSource;   //오디오 소스
+    [SerializeField] private AudioClip[] clips;         //실행할 클립들
+    [SerializeField] private BoxCollider[] colliders;   //팔에 달린 콜라이더들
+    private int collidersLength;
+    void Start()
+    {
+        collidersLength = colliders.Length;
+        SetCollider();  //콜라이더 오프
+    }
+
+    //걷는 소리 출력
     public void WalkSound()
     {
-        walkAudio.Play();
+        audioSource.PlayOneShot(clips[0], 1f);
+    }
+
+    //공격 시에 소리 출력
+    public void AttackSound()
+    {
+        audioSource.PlayOneShot(clips[1], 1f);
+    }
+
+    //공격 시작, 끝에 콜라이더 켜고 끄기
+    public void SetCollider()
+    {
+        for(int i=0; i<collidersLength; i++)
+        {
+            colliders[i].enabled = colliders[i].enabled ? false : true;
+        }
+    }
+
+    //어지러워하는 사운드 출력
+    public void DizzySound()
+    {
+        audioSource.PlayOneShot(clips[2], 1f);
     }
 }
