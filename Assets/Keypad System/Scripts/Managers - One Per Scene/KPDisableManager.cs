c09ccaaf1ed9c814e8fb7bbf3b9607485a79eb75
김승중 <31948929+pow8956@users.noreply.github.com>
2021-05-08 22:04:16 +0900

    KPDisable 수정
    기존에 플레이어가 움직이는 상태로 KPDisable을 사용하면 조종하는 것은 막혔는데 오브젝트가 안 멈추고 계속 움직이는 문제를 해결했습니다.
    이제 KPDisable을 사용하면 플레이어 오브젝트는 가만히 서있는 모션으로 바뀝니다.

--- Assets/Keypad System/Scripts/Managers - One Per Scene/KPDisableManager.cs
+++ Assets/Keypad System/Scripts/Managers - One Per Scene/KPDisableManager.cs
@@ -23,7 +23,7 @@ namespace keypadSystem
             Behaviour playerBehaviour;
             
             NetGamePlayer netGamePlayer = player.GetComponent<NetGamePlayer>();
-            
+            Animator animator = player.GetComponentInChildren<Animator>();
             if (netGamePlayer == null)
             {
                 playerBehaviour = player.GetComponent<FirstPersonController>();
@@ -36,6 +36,10 @@ namespace keypadSystem
             
             if (disable)
             {
+                animator.SetFloat("forward", 0.0f);
+                animator.SetTrigger("Exit");
+                animator.Rebind();
+                //animator.speed = 0.0f;
                 playerBehaviour.enabled = false;
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
@@ -44,6 +48,7 @@ namespace keypadSystem
 
             if (!disable)
             {
+                //animator.speed = 1.0f;
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
                 playerBehaviour.enabled = true;
